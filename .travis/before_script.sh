812782c2e585e9f995f0a3928c4cebb1a359ba7e
David Davis <daviddavis@redhat.com>
2018-03-09 15:14:17 -0500

    Using server.yaml file to set options

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env sh
 set -v
 
-# add secret key to django settings.py
-echo "SECRET_KEY = '$(cat /dev/urandom | tr -dc 'a-z0-9!@#$%^&*(\-_=+)' | head -c 50)'" >> pulp/pulpcore/pulpcore/app/settings.py
-
 if [ "$DB" = 'postgres' ]; then
-  sed -i "s/'ENGINE': 'django.db.backends.sqlite3'/'ENGINE': 'django.db.backends.postgresql_psycopg2'/" pulp/pulpcore/pulpcore/app/settings.py
-  sed -i "s/'USER': ''/'USER': 'pulp'/" pulp/pulpcore/pulpcore/app/settings.py
-  sed -i "s/\/var\/lib\/pulp\/sqlite3.db/pulp/" pulp/pulpcore/pulpcore/app/settings.py
   psql -U postgres -c 'CREATE USER pulp WITH SUPERUSER LOGIN;'
   psql -U postgres -c 'CREATE DATABASE pulp OWNER pulp;'
 fi
@@ -17,5 +11,15 @@ cp pulp/.travis/pulp-smash-config.json ~/.config/pulp_smash/settings.json
 
 sudo mkdir -p /var/lib/pulp/tmp
 sudo mkdir /var/cache/pulp
+sudo mkdir /etc/pulp/
 sudo chown -R travis:travis /var/lib/pulp
 sudo chown travis:travis /var/cache/pulp
+
+if [ "$DB" = 'postgres' ]; then
+  sudo cp pulp/.travis/server.postgres.yaml /etc/pulp/server.yaml
+else
+  # docs job also requires server.yaml
+  sudo cp pulp/.travis/server.sqlite.yaml /etc/pulp/server.yaml
+fi
+
+echo "SECRET_KEY: \"$(cat /dev/urandom | tr -dc 'a-z0-9!@#$%^&*(\-_=+)' | head -c 50)\"" | sudo tee -a /etc/pulp/server.yaml
