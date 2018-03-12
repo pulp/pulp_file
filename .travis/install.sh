b7d38c83cfaa6d3b40df91f6e0ae2a15f400b807
David Davis <daviddavis@redhat.com>
2018-03-12 16:02:30 -0400

    Run flake8 and fix errors

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 set -v
 
-# dev_requirements should not be needed for testing; don't install them to make sure
-pip install pytest git+https://github.com/PulpQE/pulp-smash.git#egg=pulp-smash
-git clone -b 3.0-dev https://github.com/pulp/pulp.git
+pip install flake8 pytest git+https://github.com/PulpQE/pulp-smash.git#egg=pulp-smash
+
+cd .. && git clone -b 3.0-dev https://github.com/pulp/pulp.git
 pushd pulp/common/ && pip install -e . && popd
 pushd pulp/pulpcore/ && pip install -e . && popd
 pushd pulp/plugin/ && pip install -e .  && popd
+
+cd pulp_file
 pip install -e .
