b2034456d5aa0fd71f8251e2e5d0a5c8cfc64813
Daniel Alley <dalley@redhat.com>
2018-07-23 16:26:25 -0400

    Move pulp-smash tests into the developer repo

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,9 +3,8 @@ set -v
 
 export COMMIT_MSG=$(git show HEAD^2 -s)
 export PULP_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp\/pull\/(\d+)' | awk -F'/' '{print $7}')
-export PULP_SMASH_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/PulpQE\/pulp-smash\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
-pip install flake8 pytest
+pip install -r test_requirements.txt
 
 cd .. && git clone https://github.com/pulp/pulp.git
 
@@ -19,18 +18,6 @@ fi
 pushd pulp/common/ && pip install -e . && popd
 pushd pulp/pulpcore/ && pip install -e . && popd
 pushd pulp/plugin/ && pip install -e .  && popd
-
-if [ -z $PULP_SMASH_PR_NUMBER ]; then
-  pip install git+https://github.com/PulpQE/pulp-smash.git#egg=pulp-smash
-else
-  git clone https://github.com/PulpQE/pulp-smash.git
-
-  pushd pulp-smash
-  git fetch origin +refs/pull/$PULP_SMASH_PR_NUMBER/merge
-  git checkout FETCH_HEAD
-  pip install -e .
-  popd
-fi
 
 cd pulp_file
 pip install -e .
---
+++ pulp_file/tests/functional/api/test_download_content.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+"""Tests that verify download of content served by Pulp."""
+import hashlib
+import unittest
+from random import choice
+from urllib.parse import urljoin
+
+from pulp_smash import api, config, selectors, utils
+from pulp_smash.pulp3.constants import DISTRIBUTION_PATH, REPO_PATH
+from pulp_smash.pulp3.utils import (
+    gen_distribution,
+    gen_remote,
+    gen_repo,
+    publish,
+    sync,
+)
+
+from pulp_file.tests.functional.api.utils import (
+    gen_publisher,
+    get_content_unit_paths,
+)
+from pulp_file.tests.functional.constants import (
+    FILE_FIXTURE_URL,
+    FILE_REMOTE_PATH,
+    FILE_PUBLISHER_PATH,
+)
+from pulp_file.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+
+class DownloadContentTestCase(unittest.TestCase):
+    """Verify whether content served by pulp can be downloaded."""
+
+    def test_all(self):
+        """Verify whether content served by pulp can be downloaded.
+
+        The process of publishing content is more involved in Pulp 3 than it
+        was under Pulp 2. Given a repository, the process is as follows:
+
+        1. Create a publication from the repository. (The latest repository
+           version is selected if no version is specified.) A publication is a
+           repository version plus metadata.
+        2. Create a distribution from the publication. The distribution defines
+           at which URLs a publication is available, e.g.
+           ``http://example.com/content/foo/`` and
+           ``http://example.com/content/bar/``.
+
+        Do the following:
+
+        1. Create, populate, publish, and distribute a repository.
+        2. Select a random content unit in the distribution. Download that
+           content unit from Pulp, and verify that the content unit has the
+           same checksum when fetched directly from Pulp-Fixtures.
+
+        This test targets the following issues:
+
+        * `Pulp #2895 <https://pulp.plan.io/issues/2895>`_
+        * `Pulp Smash #872 <https://github.com/PulpQE/pulp-smash/issues/872>`_
+        """
+        cfg = config.get_config()
+        if not selectors.bug_is_fixed(3502, cfg.pulp_version):
+            self.skipTest('https://pulp.plan.io/issues/3502')
+
+        client = api.Client(cfg, api.json_handler)
+        repo = client.post(REPO_PATH, gen_repo())
+        self.addCleanup(client.delete, repo['_href'])
+        body = gen_remote(urljoin(FILE_FIXTURE_URL, 'PULP_MANIFEST'))
+        remote = client.post(FILE_REMOTE_PATH, body)
+        self.addCleanup(client.delete, remote['_href'])
+        sync(cfg, remote, repo)
+        repo = client.get(repo['_href'])
+
+        # Create a publisher.
+        publisher = client.post(FILE_PUBLISHER_PATH, gen_publisher())
+        self.addCleanup(client.delete, publisher['_href'])
+
+        # Create a publication.
+        publication = publish(cfg, publisher, repo)
+        self.addCleanup(client.delete, publication['_href'])
+
+        # Create a distribution.
+        body = gen_distribution()
+        body['publication'] = publication['_href']
+        distribution = client.post(DISTRIBUTION_PATH, body)
+        self.addCleanup(client.delete, distribution['_href'])
+
+        # Pick a file, and download it from both Pulp Fixtures…
+        unit_path = choice(get_content_unit_paths(repo))
+        fixtures_hash = hashlib.sha256(
+            utils.http_get(urljoin(FILE_FIXTURE_URL, unit_path))
+        ).hexdigest()
+
+        # …and Pulp.
+        client.response_handler = api.safe_handler
+
+        unit_url = cfg.get_hosts('api')[0].roles['api']['scheme']
+        unit_url += '://' + distribution['base_url'] + '/'
+        unit_url = urljoin(unit_url, unit_path)
+
+        pulp_hash = hashlib.sha256(client.get(unit_url).content).hexdigest()
+        self.assertEqual(fixtures_hash, pulp_hash)
