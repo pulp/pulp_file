02e79ac27f12eca7ef1fc32ba8e7659066e4d3b1
koliveir <koliveir@redhat.com>
2019-02-13 17:40:28 -0500

    Add test to publish a lazy synced repo
    Add tests to verify that a lazy synced repository can be published.
    Rename `SyncDownloadPolicyTestCase` to
    `SyncPublishDownloadPolicyTestCase` and dd the publish test scenario.
    
    https://pulp.plan.io/issues/4418
    ref: #4418

--- pulp_file/tests/functional/api/test_download_policies.py
+++ pulp_file/tests/functional/api/test_download_policies.py
@@ -8,40 +8,56 @@ from pulp_smash.pulp3.utils import (
     gen_repo,
     get_added_content_summary,
     get_content_summary,
+    publish,
     sync,
 )
 
 from pulp_file.tests.functional.constants import (
     FILE_FIXTURE_SUMMARY,
-    FILE_REMOTE_PATH
+    FILE_PUBLISHER_PATH,
+    FILE_REMOTE_PATH,
 )
-from pulp_file.tests.functional.utils import gen_file_remote
+from pulp_file.tests.functional.utils import (
+    gen_file_publisher,
+    gen_file_remote,
+)
 from pulp_file.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
 
 
-class SyncDownloadPolicyTestCase(unittest.TestCase):
-    """Sync a repository with different download policies.
+class SyncPublishDownloadPolicyTestCase(unittest.TestCase):
+    """Sync/Publish a repository with different download policies.
 
-    This test targets the following issue:
+    This test targets the following issues:
 
     `Pulp #4126 <https://pulp.plan.io/issues/4126>`_
+    `Pulp #4418 <https://pulp.plan.io/issues/4418>`_
     """
 
     @classmethod
     def setUpClass(cls):
         """Create class-wide variables."""
         cls.cfg = config.get_config()
-        cls.client = api.Client(cls.cfg, api.json_handler)
+        cls.client = api.Client(cls.cfg, api.page_handler)
 
     def test_on_demand(self):
-        """Sync with ``on_demand`` download policy. See :meth:`do_test`."""
-        self.do_test('on_demand')
+        """Sync and publish with ``on_demand`` download policy.
+
+        See :meth:`do_sync`.
+        See :meth:`do_publish`.
+        """
+        self.do_sync('on_demand')
+        self.do_publish('on_demand')
 
     def test_streamed(self):
-        """Sync with ``streamend`` download policy.  See :meth:`do_test`."""
-        self.do_test('streamed')
+        """Sync and publish with ``streamend`` download policy.
 
-    def do_test(self, download_policy):
+        See :meth:`do_sync`.
+        See :meth:`do_publish`.
+        """
+        self.do_sync('streamed')
+        self.do_publish('streamed')
+
+    def do_sync(self, download_policy):
         """Sync repositories with the different ``download_policy``.
 
         Do the following:
@@ -86,3 +102,21 @@ class SyncDownloadPolicyTestCase(unittest.TestCase):
         self.assertNotEqual(latest_version_href, repo['_latest_version_href'])
         self.assertDictEqual(get_content_summary(repo), FILE_FIXTURE_SUMMARY)
         self.assertDictEqual(get_added_content_summary(repo), {})
+
+    def do_publish(self, download_policy):
+        """Publish repository synced with lazy download policy."""
+        repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, repo['_href'])
+
+        body = gen_file_remote(policy=download_policy)
+        remote = self.client.post(FILE_REMOTE_PATH, body)
+        self.addCleanup(self.client.delete, remote['_href'])
+
+        sync(self.cfg, remote, repo)
+        repo = self.client.get(repo['_href'])
+
+        publisher = self.client.post(FILE_PUBLISHER_PATH, gen_file_publisher())
+        self.addCleanup(self.client.delete, publisher['_href'])
+
+        publication = publish(self.cfg, publisher, repo)
+        self.assertIsNotNone(publication['repository_version'], publication)
