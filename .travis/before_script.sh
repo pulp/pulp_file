1c72a1502846238543127ba6f7baef3d96bb932b
Dennis Kliban <dkliban@redhat.com>
2019-04-30 13:44:31 -0400

    Problem: pulp not provisioned during gem publish step
    Solution: install pulp in the install install script
    
    This patch updates how Pulp is installed by Travis. It is now installed using
    ansible-pulp. The install step now happens as part of the install script.
    
    This also adds MariaDB testing.
    
    re: #4695
    https://pulp.plan.io/issues/4695

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env sh
 set -v
 
-psql -U postgres -c 'CREATE USER pulp WITH SUPERUSER LOGIN;'
-psql -U postgres -c 'CREATE DATABASE pulp OWNER pulp;'
-
 mkdir -p ~/.config/pulp_smash
 cp ../pulpcore/.travis/pulp-smash-config.json ~/.config/pulp_smash/settings.json
-
-sudo mkdir -p /var/lib/pulp/tmp
-sudo mkdir /var/cache/pulp
-sudo mkdir /etc/pulp/
-sudo chown -R travis:travis /var/lib/pulp
-sudo chown travis:travis /var/cache/pulp
-
-echo "SECRET_KEY: \"$(cat /dev/urandom | tr -dc 'a-z0-9!@#$%^&*(\-_=+)' | head -c 50)\"" | sudo tee -a /etc/pulp/settings.py
