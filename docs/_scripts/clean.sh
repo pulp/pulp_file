ce5a7bdfc37624ce96f13eddec11b58cd6af1b8e
Austin Macdonald <austin@redhat.com>
2019-05-23 15:55:08 -0400

    Clarify environment for running script
    [noissue]

--- docs/_scripts/clean.sh
+++ docs/_scripts/clean.sh
@@ -2,6 +2,7 @@
 sudo systemctl stop pulp-content-app pulp-worker@1 pulp-worker@2 pulp-resource-manager pulp-api
 
 # Reset the database (destructive!)
+# django-admin command must be run in the python environment in which pulp is installed.
 django-admin reset_db --noinput
 django-admin migrate
 django-admin reset-admin-password --password password
