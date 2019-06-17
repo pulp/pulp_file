26f8233d70cc9c1857e61074c6cac82225168664
Tatiana Tereshchenko <ttereshc@redhat.com>
2019-06-17 13:06:39 +0200

    Set version to .dev for the master branch
    A correct version would be shown for installations from the master branch.
    Bindings will be generated with a proper version as well.
    
    re #4936
    https://pulp.plan.io/issues/4936

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-file',
-    version='0.1.0b1',
+    version='0.1.0b1.dev',
     description='File plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
