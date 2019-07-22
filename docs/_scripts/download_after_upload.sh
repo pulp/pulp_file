30498dd16412fa3c1d10096e47113136bf18fe10
Mike DePaulo <mikedep333@redhat.com>
2019-07-22 14:06:05 -0400

    Handle CONTENT_HOST not being set at install time
    [noissue]

--- docs/_scripts/download_after_upload.sh
+++ docs/_scripts/download_after_upload.sh
@@ -3,6 +3,12 @@
 # The distribution will return a url that can be used by http clients
 echo "Setting DISTRIBUTION_BASE_URL, which is used to retrieve content from the content app."
 export DISTRIBUTION_BASE_URL=$(http $BASE_ADDR$DISTRIBUTION_HREF | jq -r '.base_url')
+# If Pulp was installed without CONTENT_HOST set, it's just the path.
+# And httpie will default to localhost:80
+if [[ "${DISTRIBUTION_BASE_URL:0:1}" = "/" ]]; then
+    DISTRIBUTION_BASE_URL=$CONTENT_ADDR$DISTRIBUTION_BASE_URL
+fi
 
 echo "Downloading file from Distribution via the content app."
-http -d http://$DISTRIBUTION_BASE_URL/$ARTIFACT_RELATIVE_PATH
+# This will default to http://
+http -d $DISTRIBUTION_BASE_URL/$ARTIFACT_RELATIVE_PATH
