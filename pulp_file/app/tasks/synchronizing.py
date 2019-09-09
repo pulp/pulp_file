64c14710fd02f71e0ebf4bfb81c358d53159da57
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-09-09 18:51:19 -0300

    adding code on progress bar
    ref #5184
    https://pulp.plan.io/issues/5184
    Required PR: https://github.com/pulp/pulpcore-plugin/pull/123
    Required PR: https://github.com/pulp/pulpcore/pull/289

--- pulp_file/app/tasks/synchronizing.py
+++ pulp_file/app/tasks/synchronizing.py
@@ -66,14 +66,14 @@ class FileFirstStage(Stage):
         Build and emit `DeclarativeContent` from the Manifest data.
         """
         deferred_download = self.remote.policy != Remote.IMMEDIATE  # Interpret download policy
-        with ProgressBar(message="Downloading Metadata") as pb:
+        with ProgressBar(message="Downloading Metadata", code="downloading.metadata") as pb:
             parsed_url = urlparse(self.remote.url)
             root_dir = os.path.dirname(parsed_url.path)
             downloader = self.remote.get_downloader(url=self.remote.url)
             result = await downloader.run()
             pb.increment()
 
-        with ProgressBar(message="Parsing Metadata Lines") as pb:
+        with ProgressBar(message="Parsing Metadata Lines", code="parsing.metadata") as pb:
             manifest = Manifest(result.path)
             pb.total = manifest.count()
             pb.save()
