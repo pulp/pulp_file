e0c287c79f9cdc491f44ea8c9c045bbe5a2ce262
Brian Bouterse <bmbouter@redhat.com>
2020-01-31 07:33:07 -0500

    Version bump to 0.1.1
    [noissue]

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup, find_packages
 
-requirements = ["pulpcore>=3.0.0rc8,<3.2"]
+requirements = ["pulpcore>=3.0,<3.2"]
 
 with open("README.rst") as f:
     long_description = f.read()
 
 setup(
     name="pulp-file",
-    version="0.2.0.dev",
+    version="0.1.1",
     description="File plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
