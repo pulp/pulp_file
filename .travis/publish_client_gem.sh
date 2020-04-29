06567b7cfa7d5eaee5c6cd5cc14bda6d32c66053
Matthias Dellweg <mdellweg@redhat.com>
2020-04-29 14:29:11 +0200

    Update travis config
    [noissue]

--- .travis/publish_client_gem.sh
+++ .travis/publish_client_gem.sh
@@ -13,11 +13,8 @@ echo "---
 :rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
 sudo chmod 600 ~/.gem/credentials
 
-django-admin runserver 24817 >> ~/django_runserver.log 2>&1 &
-sleep 5
-
 cd $TRAVIS_BUILD_DIR
-export REPORTED_VERSION=$(http :24817/pulp/api/v3/status/ | jq --arg plugin pulp_file -r '.versions[] | select(.component == $plugin) | .version')
+export REPORTED_VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin pulp_file -r '.versions[] | select(.component == $plugin) | .version')
 export DESCRIPTION="$(git describe --all --exact-match `git rev-parse HEAD`)"
 if [[ $DESCRIPTION == 'tags/'$REPORTED_VERSION ]]; then
   export VERSION=${REPORTED_VERSION}
@@ -38,12 +35,10 @@ then
   exit
 fi
 
-cd
-git clone https://github.com/pulp/pulp-openapi-generator.git
-cd pulp-openapi-generator
+cd "${TRAVIS_BUILD_DIR}"/../pulp-openapi-generator
 
 ./generate.sh pulp_file ruby $VERSION
 cd pulp_file-client
 gem build pulp_file_client
-GEM_FILE="$(ls | grep pulp_file_client-)"
+GEM_FILE="$(ls pulp_file_client-*)"
 gem push ${GEM_FILE}
