ae3022e0854b294c3637842e243c124aac04fb36
Dennis Kliban <dkliban@redhat.com>
2020-05-27 16:54:07 -0400

    Releasing 1.0.0
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulp-file",
-    version="1.0.0.dev",
+    version="1.0.0",
     description="File plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
