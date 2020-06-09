75657486d5297e69c8b45681405677a7159206ff
David Davis <daviddavis@redhat.com>
2020-06-09 11:39:49 -0400

    Move off fedorapeople.org fixtures
    fixes #6653

--- docs/_scripts/remote.sh
+++ docs/_scripts/remote.sh
@@ -2,7 +2,7 @@
 echo "Creating a remote that points to an external source of files."
 http POST $BASE_ADDR/pulp/api/v3/remotes/file/file/ \
     name='bar' \
-    url='https://repos.fedorapeople.org/pulp/pulp/demo_repos/test_file_repo/PULP_MANIFEST'
+    url='https://fixtures.pulpproject.org/file/PULP_MANIFEST'
 
 echo "Export an environment variable for the new remote URI."
 export REMOTE_HREF=$(http $BASE_ADDR/pulp/api/v3/remotes/file/file/ | jq -r '.results[] | select(.name == "bar") | .pulp_href')
