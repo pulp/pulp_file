5c942a239c802c4c8235151d4759adef83bee992
David Davis <daviddavis@redhat.com>
2020-09-17 12:08:47 -0400

    Adding docs for file system exporter
    fixes #7515

---
+++ docs/_scripts/exporter.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export EXPORTER_NAME=$(head /dev/urandom | tr -dc a-z | head -c5)
+export DEST_DIR=$(mktemp -d -t export-XXXXXXXX)
+
+echo "Created a new file system exporter $EXPORTER_NAME."
+export EXPORTER_HREF=$(http POST $BASE_ADDR/pulp/api/v3/exporters/file/filesystem/ \
+  name=$EXPORTER_NAME path=$DEST_DIR | jq -r '.pulp_href')
