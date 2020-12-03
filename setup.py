e32c866c04a27137b7f23077939e882cc2fccfc9
Dennis Kliban <dkliban@redhat.com>
2020-12-03 15:55:31 -0500

    Bump to 1.5.0.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulp-file",
-    version="1.4.0.post0",
+    version="1.5.0.dev",
     description="File plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
