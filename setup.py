134c347061305b4ff8347b0ca23440f1f94dd45a
Dennis Kliban <dkliban@redhat.com>
2020-12-07 12:54:14 -0500

    Release 1.4.0.post1
    Redmine Query: https://pulp.plan.io/issues?set_filter=1&status_id=*&issue_id=7437,7685
    Redmine Milestone: https://pulp.plan.io/versions/163.json
    
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulp-file",
-    version="1.4.1.dev",
+    version="1.4.0.post1",
     description="File plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
