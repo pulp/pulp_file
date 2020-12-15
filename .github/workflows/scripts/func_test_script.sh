3cc924ef300402c6cb64315db11b1c8f65b588cd
Dennis Kliban <dkliban@redhat.com>
2020-12-15 18:04:26 -0500

    Run pulpcore tests from a tag when releasing
    [noissue]

--- .github/workflows/scripts/func_test_script.sh
+++ .github/workflows/scripts/func_test_script.sh
@@ -4,4 +4,14 @@
 set -mveuo pipefail
 
 pytest -v -r sx --color=yes --pyargs pulp_file.tests.functional || show_logs_and_return_non_zero
+
+if [ "${GITHUB_REF##refs/tags/}" != "${GITHUB_REF}" ]
+then
+  PULPCORE_VERSION=$(http http://pulp/pulp/api/v3/status/ | jq -r '.versions'[0].version)
+  cd ../pulpcore
+  git fetch origin refs/tags/${PULPCORE_VERSION}
+  git checkout FETCH_HEAD
+  cd ../pulp_file
+fi
+
 pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional || show_logs_and_return_non_zero
