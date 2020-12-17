987a932ec04bd61c456beda7c6fe885e987f2a12
Dennis Kliban <dkliban@redhat.com>
2020-12-17 10:17:07 -0500

    Temporarily downgrade pulp-smash to run pulpcore 3.9 test in release workflow
    [noissue]

--- .github/workflows/scripts/func_test_script.sh
+++ .github/workflows/scripts/func_test_script.sh
@@ -12,6 +12,11 @@ then
   git fetch origin refs/tags/${PULPCORE_VERSION}
   git checkout FETCH_HEAD
   cd ../pulp_file
+  if [ ${PULPCORE_VERSION::3} == "3.9" ]
+  then
+    # Temporarily need to downgrade pulp-smash to run pulpcore 3.9 tests
+    pip install 'pulp-smash==1!0.12.0'
+  fi
 fi
 
 pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional || show_logs_and_return_non_zero
