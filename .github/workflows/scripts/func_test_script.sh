66aa494c1df879957b196f309d4485647425232e
Dennis Kliban <dkliban@redhat.com>
2021-01-07 16:14:09 -0500

    Allow the downgrade of pulp-smash for PRs and not just tagged builds.
    This is only needed when testing with pulpcore 3.9.
    
    [noissue]

--- .github/workflows/scripts/func_test_script.sh
+++ .github/workflows/scripts/func_test_script.sh
@@ -5,18 +5,20 @@ set -mveuo pipefail
 
 pytest -v -r sx --color=yes --pyargs pulp_file.tests.functional || show_logs_and_return_non_zero
 
+PULPCORE_VERSION=$(http http://pulp/pulp/api/v3/status/ | jq -r '.versions'[0].version)
+
 if [ "${GITHUB_REF##refs/tags/}" != "${GITHUB_REF}" ]
 then
-  PULPCORE_VERSION=$(http http://pulp/pulp/api/v3/status/ | jq -r '.versions'[0].version)
   cd ../pulpcore
   git fetch origin refs/tags/${PULPCORE_VERSION}
   git checkout FETCH_HEAD
   cd ../pulp_file
-  if [ ${PULPCORE_VERSION::3} == "3.9" ]
-  then
-    # Temporarily need to downgrade pulp-smash to run pulpcore 3.9 tests
-    pip install 'pulp-smash==1!0.12.0'
-  fi
+fi
+
+if [ ${PULPCORE_VERSION::3} == "3.9" ]
+then
+  # Temporarily need to downgrade pulp-smash to run pulpcore 3.9 tests
+  pip install 'pulp-smash==1!0.12.0'
 fi
 
 pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional || show_logs_and_return_non_zero
