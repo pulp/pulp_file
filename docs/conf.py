f469f53b5c87334a421c3a79e8bdb0c5975a9452
David Davis <daviddavis@redhat.com>
2021-01-27 08:51:38 -0500

    Use ReadTheDocs theme for sphinx docs
    fixes #8165

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))  # noqa
 
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    sphinx_rtd_theme = False
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -93,7 +99,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme' if sphinx_rtd_theme else 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -101,7 +107,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()] if sphinx_rtd_theme else []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
