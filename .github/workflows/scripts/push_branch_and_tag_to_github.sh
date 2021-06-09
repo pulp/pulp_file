433df08e95c678d6d577cc0dca4f5e1a9a983fb1
Dennis Kliban <dkliban@redhat.com>
2021-06-09 17:14:21 -0400

    Form the push command using username and token
    [noissue]

--- .github/workflows/scripts/push_branch_and_tag_to_github.sh
+++ .github/workflows/scripts/push_branch_and_tag_to_github.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
-export BRANCH_NAME=$(echo $GITHUB_REF | sed -rn 's/refs\/heads\/(.*)/\1/p')
+BRANCH_NAME=$(echo $GITHUB_REF | sed -rn 's/refs\/heads\/(.*)/\1/p')
 
 ref_string=$(git show-ref --tags | grep refs/tags/$1)
 
-export SHA=${ref_string:0:40}
+SHA=${ref_string:0:40}
 
-git push origin $BRANCH_NAME
+remote_repo=https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+
+git push "${remote_repo}" $BRANCH_NAME
 
 curl -s -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs \
 -H "Authorization: token $GITHUB_TOKEN" \
