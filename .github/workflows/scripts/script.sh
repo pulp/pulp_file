bc8972c34a2bc9cf1ad5cbcf1b1faf7a1108047e
Dennis Kliban <dkliban@redhat.com>
2021-06-28 09:10:38 -0400

    Update GHA config with latest from plugin_template
    [noissue]

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -128,9 +128,11 @@ if [[ "$TEST" == "upgrade" ]]; then
   # Restarting single container services
   cmd_prefix bash -c "s6-svc -r /var/run/s6/services/pulpcore-api"
   cmd_prefix bash -c "s6-svc -r /var/run/s6/services/pulpcore-content"
-  cmd_prefix bash -c "s6-svc -r /var/run/s6/services/pulpcore-resource-manager"
-  cmd_prefix bash -c "s6-svc -r /var/run/s6/services/pulpcore-worker@1"
-  cmd_prefix bash -c "s6-svc -r /var/run/s6/services/pulpcore-worker@2"
+  cmd_prefix bash -c "s6-svc -d /var/run/s6/services/pulpcore-resource-manager"
+  cmd_prefix bash -c "s6-svc -d /var/run/s6/services/pulpcore-worker@1"
+  cmd_prefix bash -c "s6-svc -d /var/run/s6/services/pulpcore-worker@2"
+  cmd_prefix bash -c "s6-svc -u /var/run/s6/services/new-pulpcore-worker@1"
+  cmd_prefix bash -c "s6-svc -u /var/run/s6/services/new-pulpcore-worker@2"
 
   echo "Restarting in 60 seconds"
   sleep 60
