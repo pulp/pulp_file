d190acf4fed27665897deb308addb4ec1d20fa48
David Davis <daviddavis@redhat.com>
2021-09-16 11:37:20 -0400

    Add ACS functional tests
    fixes #9357
    Required PR: https://github.com/pulp/pulpcore/pull/1629

--- pulp_file/tests/functional/api/test_acs.py
+++ pulp_file/tests/functional/api/test_acs.py
@@ -1,15 +1,36 @@
 import unittest
+from random import choice
 
 from pulp_smash import config
-from pulp_smash.pulp3.bindings import delete_orphans
-
-from pulpcore.client.pulp_file import AcsFileApi, RemotesFileApi
+from pulp_smash.pulp3.bindings import (
+    PulpTaskError,
+    delete_orphans,
+    monitor_task,
+    monitor_task_group,
+)
+from pulp_smash.pulp3.utils import download_content_unit, gen_distribution
+from pulpcore.client.pulp_file import (
+    AcsFileApi,
+    DistributionsFileApi,
+    FileFilePublication,
+    PublicationsFileApi,
+    RemotesFileApi,
+    RepositoriesFileApi,
+    RepositorySyncURL,
+)
 from pulpcore.client.pulp_file.exceptions import ApiException
-
 from pulpcore.tests.functional.api.using_plugin.utils import (
     gen_file_client,
     gen_file_remote,
+    gen_repo,
 )
+
+from pulp_file.tests.functional.constants import (
+    FILE_FIXTURE_MANIFEST_URL,
+    FILE_MANIFEST_ONLY_FIXTURE_URL,
+    PULP_FIXTURES_BASE_URL,
+)
+from pulp_file.tests.functional.utils import get_file_content_paths
 
 
 class AlternateContentSourceTestCase(unittest.TestCase):
@@ -25,8 +46,11 @@ class AlternateContentSourceTestCase(unittest.TestCase):
         """
         cls.cfg = config.get_config()
         cls.file_client = gen_file_client()
+        cls.repo_api = RepositoriesFileApi(cls.file_client)
         cls.file_remote_api = RemotesFileApi(cls.file_client)
         cls.file_acs_api = AcsFileApi(cls.file_client)
+        cls.publication_api = PublicationsFileApi(cls.file_client)
+        cls.distribution_api = DistributionsFileApi(cls.file_client)
         cls.paths = ["goodpath/PULP_MANIFEST", "test", "whatever/test"]
 
     @classmethod
@@ -34,24 +58,111 @@ class AlternateContentSourceTestCase(unittest.TestCase):
         """Run orphan cleanup."""
         delete_orphans()
 
-    def test_path_validation(self):
-        """Test the validation of paths."""
-        remote = self.file_remote_api.create(gen_file_remote(policy="on_demand"))
+    def _create_acs(self, name="file_acs", paths=None, remote_url=FILE_FIXTURE_MANIFEST_URL):
+        remote = self.file_remote_api.create(gen_file_remote(remote_url, policy="on_demand"))
         self.addCleanup(self.file_remote_api.delete, remote.pulp_href)
 
-        # one path is wrong, begins with /
         acs_data = {
-            "name": "alternatecontentsource",
+            "name": name,
             "remote": remote.pulp_href,
-            "paths": self.paths + ["/bad_path"],
         }
+        if paths:
+            acs_data["paths"] = paths
 
+        acs = self.file_acs_api.create(acs_data)
+        self.addCleanup(self.file_acs_api.delete, acs.pulp_href)
+
+        return acs
+
+    def test_path_validation(self):
+        """Test the validation of paths."""
+        # path is wrong, begins with /
         with self.assertRaises(ApiException) as ctx:
-            acs = self.file_acs_api.create(acs_data)
+            self._create_acs(paths=(self.paths + ["/bad_path"]))
         self.assertEqual(ctx.exception.status, 400)
 
         # use valid paths
-        acs_data["paths"] = self.paths
-        acs = self.file_acs_api.create(acs_data)
-        self.addCleanup(self.file_acs_api.delete, acs.pulp_href)
+        acs = self._create_acs(paths=self.paths)
         self.assertEqual(sorted(acs.paths), sorted(self.paths))
+
+    def test_acs_sync(self):
+        """Test syncing from an ACS."""
+        repo = self.repo_api.create(gen_repo())
+        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+
+        remote = self.file_remote_api.create(gen_file_remote(FILE_MANIFEST_ONLY_FIXTURE_URL))
+        self.addCleanup(self.file_remote_api.delete, remote.pulp_href)
+
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+
+        # sync should fail as the repo has metadata only (no files)
+        sync_response = self.repo_api.sync(repo.pulp_href, repository_sync_data)
+        with self.assertRaises(PulpTaskError) as ctx:
+            monitor_task(sync_response.task)
+        self.assertIn("404", ctx.exception.task.error["description"])
+
+        # create an acs and pull in its remote artifacts
+        acs = self._create_acs()
+        resp = self.file_acs_api.refresh(acs.pulp_href, acs)
+        monitor_task_group(resp.task_group)
+
+        # the sync show now work as the files are being pulled from ACS remote
+        sync_response = self.repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+
+    def test_acs_sync_with_paths(self):
+        """Test syncing from an ACS using different paths."""
+        repo = self.repo_api.create(gen_repo())
+        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+
+        remote = self.file_remote_api.create(gen_file_remote(FILE_MANIFEST_ONLY_FIXTURE_URL))
+        self.addCleanup(self.file_remote_api.delete, remote.pulp_href)
+
+        acs = self._create_acs(
+            paths=("file/PULP_MANIFEST", "file2/PULP_MANIFEST"),
+            remote_url=PULP_FIXTURES_BASE_URL,
+        )
+        resp = self.file_acs_api.refresh(acs.pulp_href, acs)
+        monitor_task_group(resp.task_group)
+
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = self.repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+
+    def test_serving_acs_content(self):
+        """Test serving of ACS content through the content app."""
+        cfg = config.get_config()
+        repo = self.repo_api.create(gen_repo())
+        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+
+        remote = self.file_remote_api.create(
+            gen_file_remote(FILE_MANIFEST_ONLY_FIXTURE_URL, policy="on_demand")
+        )
+        self.addCleanup(self.file_remote_api.delete, remote.pulp_href)
+
+        acs = self._create_acs()
+        resp = self.file_acs_api.refresh(acs.pulp_href, acs)
+        monitor_task_group(resp.task_group)
+
+        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
+        sync_response = self.repo_api.sync(repo.pulp_href, repository_sync_data)
+        monitor_task(sync_response.task)
+        repo = self.repo_api.read(repo.pulp_href)
+
+        # Create a publication.
+        publish_data = FileFilePublication(repository=repo.pulp_href)
+        publish_response = self.publication_api.create(publish_data)
+        created_resources = monitor_task(publish_response.task).created_resources
+        publication_href = created_resources[0]
+        self.addCleanup(self.publication_api.delete, publication_href)
+
+        # Create a distribution.
+        body = gen_distribution()
+        body["publication"] = publication_href
+        distribution_response = self.distribution_api.create(body)
+        created_resources = monitor_task(distribution_response.task).created_resources
+        distribution = self.distribution_api.read(created_resources[0])
+        self.addCleanup(self.distribution_api.delete, distribution.pulp_href)
+
+        unit_path = choice(get_file_content_paths(repo.to_dict()))
+        download_content_unit(cfg, distribution.to_dict(), unit_path)
