69f627f40ebd7f7945b1511172d3b1533000af0b
Gerrod Ubben <gerrodubben@gmail.com>
2022-06-07 15:50:55 -0400

    Add test for content directory listing
    [noissue]
    Required PR: https://github.com/pulp/pulpcore/pull/2516

--- pulp_file/tests/functional/api/from_pulpcore/test_content_path.py
+++ pulp_file/tests/functional/api/from_pulpcore/test_content_path.py
@@ -1,15 +1,18 @@
 """Tests related to content path."""
+import pytest
 import unittest
 
-from pulp_smash import api, config
+from pulp_smash import api, config, utils
 from pulp_smash.pulp3.bindings import delete_orphans
-from pulp_smash.pulp3.utils import gen_remote, gen_repo, sync
+from pulp_smash.pulp3.utils import gen_remote, gen_repo, sync, gen_distribution
+from urllib.parse import urljoin
 
 from pulp_file.tests.functional.utils import create_file_publication
 from .constants import (
     FILE_FIXTURE_MANIFEST_URL,
     FILE_REMOTE_PATH,
     FILE_REPO_PATH,
+    PULP_CONTENT_BASE_URL,
 )
 
 
@@ -55,3 +58,25 @@ class SyncPublishContentPathTestCase(unittest.TestCase):
             sync(cfg, remote, repo)
             repo = client.get(repo["pulp_href"])
             create_file_publication(cfg, repo)
+
+
+@pytest.mark.parallel
+def test_content_directory_listing(file_distro_api_client, gen_object_with_cleanup):
+    """Checks that distributions are grouped by base-path when listing content directories."""
+    base_path = utils.uuid4()
+    for path in ["/foo1", "/foo2", "/boo/foo1"]:
+        gen_object_with_cleanup(
+            file_distro_api_client, gen_distribution(base_path=base_path + path)
+        )
+
+    response = utils.http_get(PULP_CONTENT_BASE_URL).decode("utf-8")
+    assert response.count(f'a href="{base_path}/"') == 1
+
+    url = urljoin(PULP_CONTENT_BASE_URL, base_path + "/")
+    response = utils.http_get(url).decode("utf-8")
+    assert response.count('a href="foo1/"') == 1
+    assert response.count('a href="foo2/"') == 1
+    assert response.count('a href="boo/"') == 1
+
+    response = utils.http_get(urljoin(url, "boo/")).decode("utf-8")
+    assert response.count('a href="foo1/"') == 1
