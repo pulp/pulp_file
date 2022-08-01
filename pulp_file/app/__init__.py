457c9228f54b063a58434c83a1a61c684df33c09
pulpbot <pulp-infra@redhat.com>
2022-08-01 10:33:33 +0000

    Release 1.11.1 GH Issues:
    [noissue]

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,5 +8,5 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "1.11.1.dev"
+    version = "1.11.1"
     python_package_name = "pulp-file"
