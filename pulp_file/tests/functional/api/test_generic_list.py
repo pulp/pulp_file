2a8628c961f3836ef669ea40b75e694592c4f520
MichalPysik <n3r0x541@gmail.com>
2022-08-29 15:46:50 +0200

    Added test for listing master model remotes
    A new feature has been added to pulpcore, which allows users to list
    all master model remotes at /pulp/api/v3/remotes/. A test case
    covering this feature has been added.
    
    [noissue]
    
    Required-PR: pulp/pulpcore#3150

--- pulp_file/tests/functional/api/test_generic_list.py
+++ pulp_file/tests/functional/api/test_generic_list.py
@@ -39,3 +39,22 @@ def test_read_all_content_guards_generic(
     assert response.count != 0
     for content_guard in response.results:
         assert content_guard.name is not None
+
+
+@pytest.mark.parallel
+def test_read_all_master_model_remotes_generic(
+    remotes_api_client, gen_object_with_cleanup, file_remote_api_client
+):
+    remote_data = {"name": str(uuid.uuid4()), "url": "http://example.com"}
+    remote1 = gen_object_with_cleanup(file_remote_api_client, remote_data)
+    remote_data = {"name": str(uuid.uuid4()), "url": "http://example.org"}
+    remote2 = gen_object_with_cleanup(file_remote_api_client, remote_data)
+
+    response = remotes_api_client.list()
+    assert response.count != 0
+
+    hrefs = []
+    for remote in response.results:
+        hrefs.append(remote.pulp_href)
+    assert remote1.pulp_href in hrefs
+    assert remote2.pulp_href in hrefs
