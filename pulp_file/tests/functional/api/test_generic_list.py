a2786cfb02184f3bb6173c14df3b24e7bdf8a1fd
Matthias Dellweg <mdellweg@redhat.com>
2022-08-30 12:49:10 +0200

    Fix generic list tests
    [noissue]
    
    Required PR: https://github.com/pulp/pulpcore/pull/3150

--- pulp_file/tests/functional/api/test_generic_list.py
+++ pulp_file/tests/functional/api/test_generic_list.py
@@ -5,18 +5,18 @@ import pytest
 
 
 @pytest.mark.parallel
-def test_read_all_repos_generic(file_repo_api_client, file_repo):
+def test_read_all_repos_generic(repositories_api_client, file_repo):
     """Ensure name is displayed when listing repositories generic."""
-    response = file_repo_api_client.list()
+    response = repositories_api_client.list()
     assert response.count != 0
     for repo in response.results:
         assert repo.name is not None
 
 
 @pytest.mark.parallel
-def test_read_all_content_generic(file_content_api_client, file_random_content_unit):
+def test_read_all_content_generic(content_api_client, file_random_content_unit):
     """Ensure href is displayed when listing content generic."""
-    response = file_content_api_client.list()
+    response = content_api_client.list()
     assert response.count != 0
     for content in response.results:
         assert content.pulp_href is not None
@@ -24,11 +24,15 @@ def test_read_all_content_generic(file_content_api_client, file_random_content_u
 
 @pytest.mark.parallel
 def test_read_all_content_guards_generic(
-    content_guards_api_client, tls_certificate_authority_cert, x509_content_guards_api_client
+    gen_object_with_cleanup,
+    content_guards_api_client,
+    tls_certificate_authority_cert,
+    x509_content_guards_api_client,
 ):
     """Ensure name is displayed when listing content guards generic."""
-    x509_content_guards_api_client.create(
-        {"name": str(uuid.uuid4()), "ca_certificate": tls_certificate_authority_cert}
+    gen_object_with_cleanup(
+        x509_content_guards_api_client,
+        {"name": str(uuid.uuid4()), "ca_certificate": tls_certificate_authority_cert},
     )
 
     response = content_guards_api_client.list()
