1344338bb367916fc11e35aca77d771f0cdacd34
pulpbot <pulp-infra@redhat.com>
2023-02-03 16:01:17 +0000

    Release 1.12.0 GH Issues: 825,814,837,822,862,844,819,774,876,821,881,823,867,815,816,818,826,866,824,828,820,813,747
    [noissue]

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,5 +8,5 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "1.12.0.dev"
+    version = "1.12.0"
     python_package_name = "pulp-file"
