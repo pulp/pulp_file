59183946702fbe9c69c48ad8cc058fce406a0be0
Matthias Dellweg <mdellweg@redhat.com>
2023-03-23 15:30:08 +0100

    Update CI
    [noissue]

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -8,11 +8,11 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
+set -mveuo pipefail
+
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 REPO_ROOT="$PWD"
-
-set -mveuo pipefail
 
 source .github/workflows/scripts/utils.sh
 
@@ -44,21 +44,16 @@ if [[ "$TEST" = "docs" ]]; then
   exit
 fi
 
+REPORTED_STATUS="$(pulp status)"
+
 if [[ "${RELEASE_WORKFLOW:-false}" == "true" ]]; then
-  STATUS_ENDPOINT="${PULP_URL}${PULP_API_ROOT}api/v3/status/"
-  echo $STATUS_ENDPOINT
-  REPORTED_VERSION=$(http $STATUS_ENDPOINT | jq --arg plugin file --arg legacy_plugin pulp_file -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+  REPORTED_VERSION="$(echo $REPORTED_STATUS | jq --arg plugin file --arg legacy_plugin pulp_file -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')"
   response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-file/$REPORTED_VERSION/)
   if [ "$response" == "200" ];
   then
     echo "pulp_file $REPORTED_VERSION has already been released. Skipping running tests."
     exit
   fi
-fi
-
-if [[ "$TEST" == "plugin-from-pypi" ]]; then
-  COMPONENT_VERSION=$(http https://pypi.org/pypi/pulp-file/json | jq -r '.info.version')
-  git checkout ${COMPONENT_VERSION} -- pulp_file/tests/
 fi
 
 echo "machine pulp
@@ -69,15 +64,13 @@ password password
 cmd_user_stdin_prefix bash -c "chmod 600 ~pulp/.netrc"
 
 cd ../pulp-openapi-generator
-./generate.sh pulp_file python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulp_file-client
-sudo rm -rf ./pulp_file-client
-./generate.sh pulpcore python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulpcore-client
-sudo rm -rf ./pulpcore-client
-./generate.sh pulp_certguard python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulp_certguard-client
-sudo rm -rf ./pulp_certguard-client
+for item in $(echo "$REPORTED_STATUS" | jq -r '.versions[].package|sub("-"; "_")')
+do
+./generate.sh "${item}" python
+cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+sudo rm -rf "./${item}-client"
+done
+
 cd $REPO_ROOT
 
 cat unittest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/unittest_requirements.txt"
@@ -129,8 +122,8 @@ else
 fi
 export PULP_FIXTURES_URL="http://pulp-fixtures:8080"
 pushd ../pulp-cli
-pip install -r test_requirements.txt
-pytest -v -m pulp_file
+pip install -r test_requirements.txt pytest-custom_exit_code
+pytest --suppress-no-test-exit-code -v -m pulp_file
 popd
 
 if [ -f $POST_SCRIPT ]; then
