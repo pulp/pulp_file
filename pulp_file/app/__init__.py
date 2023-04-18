b23307ada973d69cfc446340b074a61547ec096d
pulpbot <pulp-infra@redhat.com>
2023-04-18 17:06:28 +0000

    Bump to 1.14.2.dev
    [noissue]

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,6 +8,6 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "1.14.1"
+    version = "1.14.2.dev"
     python_package_name = "pulp-file"
     domain_compatible = True
