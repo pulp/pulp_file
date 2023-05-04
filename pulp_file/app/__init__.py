e964bacd0afecd979058824bd39336f358ed8559
pulpbot <pulp-infra@redhat.com>
2023-05-04 11:49:22 +0000

    Release 1.14.2 GH Issues: 954
    [noissue]

--- pulp_file/app/__init__.py
+++ pulp_file/app/__init__.py
@@ -8,6 +8,6 @@ class PulpFilePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_file.app"
     label = "file"
-    version = "1.14.2.dev"
+    version = "1.14.2"
     python_package_name = "pulp-file"
     domain_compatible = True
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulp-file",
-    version="1.14.2.dev",
+    version="1.14.2",
     description="File plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
